300e50a24a35637f7893ea98a7ea5d9b836e5903
Mehemmed <LENOVA-NB@LENOVA>
2024-05-29 15:43:08 +0400

    finish

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,6 +9,7 @@ import { Home } from "./Companents/Home/Home";
 import { Login } from "./Companents/Login/Login";
 import { Payment } from "./Companents/Payment/Payment";
 import { Search } from "./Companents/Search/Search";
+import { PayMentCard } from "./Companents/Payment/PayMentCard";
 function App() {
   return (
     <div className="div-container">
@@ -19,6 +20,7 @@ function App() {
         <Route path="/register" element={<Login/>}/>
         <Route path="/payment" element={<Payment/>}/>
         <Route path="/search" element={<Search/>}/>
+        <Route path="/payment/pay" element={<PayMentCard/>}/>
       </Routes>
      
     </div>
